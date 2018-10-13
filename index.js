17f69d7a4966eb0686e2457ff49f37dccd063c8b
Lauren Hilton <lauren.hilton@daugherty.com>
2018-10-13 07:56:00 -0500

    add cors

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const routes = require('./base/routes.js');
+const cors = require('cors');
 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
+var whitelist = ['http://localhost:4200']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 // serve static content
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/hello', routes);
+app.use(cors(corsOptions));
 
 // Connection URL
 const url = 'mongodb://bruce:password1@ds131313.mlab.com:31313/wallaby'
