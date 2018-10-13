a6249cb7eeaf22ddadfa2fefe48e5de6b0c260ff
CORPORATE\krb0916 <kevin.blake@daugherty.com>
2018-10-12 22:17:46 -0500

    Added mongo functionality and routes

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 // include modules
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
+const routes = require('./base/routes.js');
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 // serve static content
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/hello', routes);
+
+// Connection URL
+const url = 'mongodb://bruce:password1@ds131313.mlab.com:31313/wallaby'
+ 
+// Database Name
+const dbName = 'wallaby';
+ 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+    if(err){console.log(err)};
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+ 
+  client.close();
+});
 
 // setup server
 app.listen(7777);
 console.log("Listening on port 7777");
+
