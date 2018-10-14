18cef008c9242631ae9a90088064e57864e2c11b
CORPORATE\krb0916 <kevin.blake@daugherty.com>
2018-10-13 19:22:03 -0500

    Database hookup for experiences

--- base/routes.js
+++ base/routes.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-const API_URL = 'http://localhost:4200';
+var db; 
+
+const UI_URL = 'http://localhost:4200';
 
 const options = {
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
   credentials: true,
   methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-  origin: API_URL,
+  origin: UI_URL,
   preflightContinue: false
 };
+
+
+// Connection URL
+const url = 'mongodb://bruce:password1@ds131313.mlab.com:31313/wallaby'
+ 
+// Database Name
+const dbName = 'wallaby';
+ 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+    if(err){console.log(err)};
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+ 
+  db = client.db(dbName);
+ });
 
 //use cors middleware
 router.use(cors(options));
@@ -19,10 +39,27 @@ router.use(cors(options));
 
 router.get('/', function (req, res) {
   res.send({'text':'Kangaroo home page'});
-}) ;
+});
+
+router.post('/addExperience', (req, res) => {
+  // Insert JSON straight into MongoDB
+  db.collection('experiences').insertOne(req.body, function (err, result) {
+      if (err)
+          throw err;
+      else
+        res.send('Success!!');
+  });
+});
+
+router.get('/getExperiences', (req, res) => {
+  db.collection("experiences").find({}).toArray(function(err, result) {
+    if (err)
+     throw err;
+     res.send(result);
+    });
+});
+
 //enable pre-flight
 router.options("*", cors(options));
-
-
 
 module.exports = router;
